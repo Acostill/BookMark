53fff97df1a35495284adc794ac221789425ff0e
Acostill <gersoncastillo@ac.c4q.nyc>
2018-06-07 12:58:57 -0400

    Added queries for adding media and updating media progress

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const bookmarkDb = require("../db/queries");
+const BMDb = require("../db/queries");
 const { loginRequired } = require("../auth/helpers");
 
-/* Get Requests */
-router.post('/register', bookmarkDb.authQueries.registerUser);
-router.post('/login', bookmarkDb.authQueries.loginUser);
-router.post('/logout', bookmarkDb.authQueries.logoutUser);
+/* Post Requests */
+router.get('/logout', loginRequired, BMDb.authQueries.logoutUser);
+router.get('/session_user', loginRequired, BMDb.getSessionUser);
+router.get('/username/:username', BMDb.getUserByUsername);
+
+/* Post Requests */
+router.post('/register', BMDb.authQueries.registerUser);
+router.post('/login', BMDb.authQueries.loginUser);
+
+router.post('/anime/list', BMDb.animeList);
+router.post('/anime/add', BMDb.addAnime);
+router.post('/anime/progress', BMDb.updateAnimeProgress);
+
+router.post('/series/list', BMDb.seriesList);
+router.post('/series/add', BMDb.addSeries);
+router.post('/series/progress', BMDb.updateSeriesProgress);
+
+router.post('/movie/list', BMDb.movieList);
+router.post('/movie/add', BMDb.addMovie);
+
+router.post('/book/list', BMDb.bookList);
+router.post('/book/add', BMDb.addBook);
+router.post('/book/progress', BMDb.updateBookProgress);
 
 module.exports = router;
